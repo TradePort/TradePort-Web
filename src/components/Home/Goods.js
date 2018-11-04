5d7e87fbf9e3d3e97f4f1f14792e9a1e90967e9a
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-11-04 16:00:05 +0100

    Remove background, add assets

--- src/components/Home/Goods.js
+++ src/components/Home/Goods.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
+import GoodsImage from '../../assets/images/iu-4.jpeg'
 
 const GoodsComponent = () => (
     <section className="goods-section">
         <Container>
             <Row>
-                <Col xs={12} sm={12} md={6} lg={6}></Col>
                 <Col xs={12} sm={12} md={6} lg={6}>
-                    <div className="title">
-                        <span>Goods Trade</span>
-                    </div>
-                    <div className="description">
-                        <p>
-                            By using the TradePort
-                            StandardMarketplace
-                            contract you can now
-                            create your own
-                            decentralized Amazon.
+                    <img src={GoodsImage} className="img-fluid" alt="Goods Trade" />
+                </Col>
+                <Col xs={12} sm={12} md={6} lg={6}>
+                    <div className="info">
+                        <div className="title">
+                            <span>Goods Trade</span>
+                        </div>
+                        <div className="description">
+                            <p>
+                                By using the TradePort
+                                StandardMarketplace
+                                contract you can now
+                                create your own
+                                decentralized Amazon.
                         </p>
+                        </div>
                     </div>
                 </Col>
             </Row>
--- src/components/Home/Services.js
+++ src/components/Home/Services.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
+import ServicesImage from '../../assets/images/services.png'
 
 const ServicesComponent = () => (
     <section className="services-section">
         <Container>
             <Row>
                 <Col xs={12} sm={12} md={6} lg={6}>
-                    <div className="title">
-                        <span>Services Trade</span>
-                    </div>
-                    <div className="description">
-                        <p>
-                            TradePort creates global ports
-                            of trade using mobile
-                            technologies to allow the
-                            creating of smart cities.
+                    <div className="info">
+                        <div className="title">
+                            <span>Services Trade</span>
+                        </div>
+                        <div className="description">
+                            <p>
+                                TradePort creates global ports
+                                of trade using mobile
+                                technologies to allow the
+                                creating of smart cities.
                         </p>
+                        </div>
                     </div>
                 </Col>
-                <Col xs={12} sm={12} md={6} lg={6}></Col>
+                <Col xs={12} sm={12} md={6} lg={6}>
+                    <img src={ServicesImage} className="img-fluid" alt="Services Trade" />
+                </Col>
             </Row>
         </Container>
     </section>
